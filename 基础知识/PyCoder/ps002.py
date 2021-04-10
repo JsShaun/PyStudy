f4e3a1e3fe5a08a34cb8189154ca7bea96ec5da2
Shaun <772542545@qq.com>
2021-04-10 17:31:42 +0800

    .

--- 基础知识/PyCoder/ps002.py
+++ 基础知识/PyCoder/ps002.py
@@ -1,3 +1,22 @@
 
-a, b, c, d = 20, 5.5, True, 4+3j
-print(type(a), type(b), type(c), type(d))
+# a, b, c, d = 20, 5.5, True, 4+3j
+# print(type(a), type(b), type(c), type(d))
+
+
+
+# list = [ 'abcd', 786 , 2.23, 'runoob', 70.2 ]
+# tinylist = [123, 'runoob']
+# print (list) # 输出完整列表
+# print (list[0]) # 输出列表第一个元素
+# print (list[1:3]) # 从第二个开始输出到第三个元素
+# print (list[2:]) # 输出从第三个元素开始的所有元素
+# print (tinylist * 2) # 输出两次列表
+# print (list + tinylist) # 连接列表
+
+
+
+st1 = {'Tom', 'Jerry', 'Pite', 'Tom', 'Jack', 'Rose','Shaun'}
+print(st1) # 输出集合，集合重复的元素被自动去掉
+
+st2 = set({'Tom', 'Jerry', 'Pite', 'Tom', 'Jack', 'Rose','Shaun'})
+print(st2) # 这里同等于上面
