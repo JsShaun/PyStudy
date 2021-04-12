c3bb649eff1800c17e933ac3def08e2455496401
Shaun <772542545@qq.com>
2021-04-12 09:57:09 +0800

    .

--- 基础知识/PyCoder/ps002.py
+++ 基础知识/PyCoder/ps002.py
@@ -20,3 +20,30 @@ print(st1) # 输出集合，集合重复的元素被自动去掉
 
 st2 = set({'Tom', 'Jerry', 'Pite', 'Tom', 'Jack', 'Rose','Shaun'})
 print(st2) # 这里同等于上面
+
+
+if 'Rose' in st2 :
+    print('Rose 在集合中')
+else :
+    print('Rose 不在集合中')
+
+
+a = set('abracadabra')
+b = set('alacazam')
+print(a)
+print(a - b) # a 和 b 的差集
+print(a | b) # a 和 b 的并集
+print(a & b) # a 和 b 的交集
+print(a ^ b) # a 和 b 中不同时存在的元素
+
+
+dict1 = {}
+dict1['one'] = "strFirst"
+dict1[2] = "numTwo"
+print (dict1['one']) # 输出键为 'one' 的值
+print (dict1[2]) # 输出键为 2 的值
+
+dict2 = {'name': 'huaz','code':1, 'key2': 'value2'}
+print (dict2) # 输出完整的字典
+print (dict2.keys()) # 输出所有键
+print (dict2.values()) # 输出所有值
