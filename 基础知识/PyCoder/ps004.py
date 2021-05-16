469e6ef17b58a7793b7588b8e3fdf5df96ca8bc6
Shaun <772542545@qq.com>
2021-05-16 21:31:36 +0800

    .

---
+++ 基础知识/PyCoder/ps004.py
@@ -0,0 +1,18 @@
+
+
+try:
+
+    1/0
+except ZeroDivisionError as e:
+    print("这里是除以零的捕获错误")
+    print("错误提醒：",e)
+
+except Exception as e:
+    print("这里是其它所有类型错误")
+    print("错误提醒：",e)
+    
+else:
+    print("没有错误则执行这里")
+
+finally:
+    print("无论对错，都执行")
